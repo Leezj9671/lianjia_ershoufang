e38b44c4e528a3136ce9c879a473a57d6e6ddaff
李正军 <leezj9671@gmail.com>
2017-08-16 18:13:54 +0800

    matplotlib.pyplot add
    it can show tables now

--- pic.py
+++ pic.py
@@ -1,18 +1,33 @@
 import csv
-import numpy
-import matplotlib.pyplot as pyplot
+import numpy as np
+import matplotlib.pyplot as plt
 
-price, size = numpy.loadtxt('house.csv', delimiter='|', usecols=(1, 2), unpack=True)
+price, size = np.loadtxt('house.csv', delimiter='|', usecols=(1, 2), unpack=True)
 print(price, size)
 
-price_mean = numpy.mean(price)
-size_mean = numpy.mean(size)
+price_mean = np.mean(price)
+size_mean = np.mean(size)
 
 print('average price: {}'.format(price_mean))
 print('average size: {}'.format(size_mean))
 
-price_var = numpy.var(price)
-size_var = numpy.var(size)
+price_var = np.var(price)
+size_var = np.var(size)
 
 print('var price: {}'.foramt(price_var))
 print('var size: {}'.foramt(size_var))
+
+plt.figure()
+plt.subplot(211)
+plt.title("/ 1W RMB")
+plt.hist(price, bins=20)
+
+plt.subplot(212)
+plt.xlabel("/ m^2")
+plt.hist(size, bins=20)
+
+plt.figure(2)
+plt.title("price")
+plt.plot(price)
+
+plt.show()
